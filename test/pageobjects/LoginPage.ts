283936bd242cf742dcac19a75add0175d44b73a6
Rama Raju <ramrajulenka@gamil.com>
2026-08-27 10:45:37 +0530

    New Testcased added for login page and login functionality

--- test/pageobjects/LoginPage.ts
+++ test/pageobjects/LoginPage.ts
@@ -54,6 +54,66 @@ class LoginPage {
         await this.emailField.click();
         await this.emailField.setValue(email);
     }
+
+//Verify the email toast message after entering the invalid email address
+
+get invalidEmailToast() {
+    return $('//android.widget.Toast[@text="Please enter valid email address"]');
+}
+
+async verifyInvalidEmailToast() {
+    await expect(this.invalidEmailToast).toBeDisplayed();
+   }
+    //Verify the Deshboard page after login
+    get dashboardTitle() {
+    return $('//android.widget.TextView[@text="Dashboard"]');
+}
+
+async verifyDashboard() {
+    await this.dashboardTitle.waitForDisplayed({
+        timeout: 10000
+    });
+
+    await expect(this.dashboardTitle).toBeDisplayed();
+}
+    // OTP
+
+    get verifyOtpButton() {
+        return $('~Verify OTP');
+    }
+
+    get invalidOtpMessage() {
+        return $('android=new UiSelector().textContains("Invalid OTP")');
+    }
+
+    async enterOTP(otp: string): Promise<void> {
+
+        for (let i = 0; i < otp.length; i++) {
+
+            const otpField = $(
+                `android=new UiSelector().className("android.widget.EditText").instance(${i})`
+            );
+
+            await otpField.waitForDisplayed({
+              //  timeout: 5000
+            });
+
+            await otpField.setValue(otp[i]);
+        }
+    }
+
+    async clickVerifyOTP(): Promise<void> {
+        await this.verifyOtpButton.click();
+    }
+
+    async verifyInvalidOTPMessage(): Promise<void> {
+
+        await this.invalidOtpMessage.waitForDisplayed({
+            timeout: 5000
+        });
+
+        await expect(this.invalidOtpMessage).toBeDisplayed();
+    }
 }
 
 export default new LoginPage();
--- test/specs/Login.e2e.ts
+++ test/specs/Login.e2e.ts
@@ -19,18 +19,34 @@ describe("Verify The LoginPage functionality", () => {
         await expect(LoginPage.emailField).toBeDisplayed();
     });
 
+   /* it("Verify the inavlid email toast message after entering the invalid email address", async () => {
+        await LoginPage.emailField.setValue(
+            'invalid-email'
+        );
+        await LoginPage.continueButton.click();
+        await browser.pause(2000);
+        await LoginPage.verifyInvalidEmailToast();
+        
+    });
+
+*/
     it("Verify the Terms and Privacy text", async () => {
         await expect(LoginPage.termsPrivacyText).toBeDisplayed();
     });
 
+
+
     it("Verify the valid user able to login", async () => {
         await LoginPage.emailField.setValue(
-            'ramaraju.lenka@gmail.com'
+            'iamkira134@gmail.com'
         );
-
         await LoginPage.continueButton.click();
-
         await browser.pause(15000);
-    });
+        await LoginPage.enterOTP('123456');
+        await browser.pause(15000);
+        await expect(LoginPage.dashboardTitle).toBeDisplayed();
+    
+    })
+
 
 });
